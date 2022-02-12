3ff4b65b5be6c5790a1fe6a1f01cfa28be9533e2
Akeiba <akeiba@live.com>
2022-02-12 23:52:44 +0000

    created links and basic icon styling for home page. will add functionality

--- client/src/Components/Home.js
+++ client/src/Components/Home.js
@@ -1,8 +1,14 @@
 import React from "react";
 import NavBar from "./NavBar";
+import { NavLink } from "react-router-dom";
 
 import "../Styles/Nav.css";
 import "../Styles/Home.css";
+
+// React Icons
+import { FaPlaneDeparture } from "react-icons/fa";
+import { FaBed } from "react-icons/fa";
+import { FaCar } from "react-icons/fa";
 
 // Will change, need links to load primarily
 const Home = () => {
@@ -11,8 +17,26 @@ const Home = () => {
       <NavBar />
       <div className="home-underlay">
         <div className="home-page">
-          <div className="home-page">
-            <h1 className="home-header">Home</h1>
+          <h1 className="home-header">Home</h1>
+
+          <div className="home-icons-area">
+            <NavLink to="/Flights" className="home-nav-link-path">
+              <a href="/Flights" className="home-nav-link">
+                <FaPlaneDeparture />
+              </a>
+            </NavLink>
+
+            <NavLink to="/Hotels" className="home-nav-link-path">
+              <a href="" className="home-nav-link">
+                <FaBed />
+              </a>
+            </NavLink>
+
+            <NavLink to="/Parking" className="home-nav-link-path">
+              <a href="" className="home-nav-link">
+                <FaCar />
+              </a>
+            </NavLink>
           </div>
         </div>
       </div>
