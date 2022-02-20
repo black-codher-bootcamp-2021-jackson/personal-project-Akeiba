556df22a4eedc48150c4beb516ebae9930b50c88
Akeiba <akeiba@live.com>
2022-02-20 11:00:11 +0000

    fixed react calendar selection issue

--- client/src/Components/TravelCalendar.js
+++ client/src/Components/TravelCalendar.js
@@ -7,13 +7,11 @@ import "../Styles/Calendar.css";
 ////////////////////////////////////////////////
 
 function TravelCalendar() {
-  const [date] = useState(new Date());
-  // const [date, setDate] = useState(new Date());
-
+  const [date, setDate] = useState(new Date());
   const onChange = (date) => {
     console.log("date1 = ", date[0]);
     console.log("date 2 = ", date[1]);
-    // , setDate(date);
+    setDate(date);
   };
 
   return (
@@ -36,37 +34,87 @@ function TravelCalendar() {
 
 export default TravelCalendar;
 
-// ////////////////////////////
-// ////////////////////////////
-// ////////////////////////////
+// ORIGINAL CODE
+// import React from "react";
+// import { useState } from "react";
+// import Calendar from "react-calendar";
+// import "react-calendar/dist/Calendar.css";
+
 // function TravelCalendar() {
 //   const [date, setDate] = useState(new Date());
-//   const [startDate, setStartDate] = useState("");
-//   const [endDate, setEndDate] = useState("");
-
-//   const onChange = (date) => {
-//     console.log("date1 = ", date[0]);
-//     console.log("date 2 = ", date[1]);
-//     setStartDate(date[0]);
-//     setEndDate(date[1]);
-//   };
 
 //   return (
-//     <div className="travel-calendar-section">
-//       <div className="calendar-container">
-//         <Calendar onChange={onChange} value={date} selectRange={true} />
+//     <div className="calendar">
+//       <h1 className="calendar-title">Travel Calendar</h1>
+
+//       <div>
+//         <Calendar onChange={setDate} value={date} />
 //       </div>
-//       {startDate.length > 0 && endDate.length > 0 ? (
-//         <p className="text-center">
-//           <span className="bold">Start:</span> {startDate.toDateString()}
-//           &nbsp;|&nbsp;
-//           <span className="bold">End:</span> {endDate.toDateString()}
-//         </p>
-//       ) : (
-//         <p className="text-center"></p>
-//       )}
 //     </div>
 //   );
 // }
 
 // export default TravelCalendar;
+
+// const [value, onChange] = useState(new Date());
+
+// return (
+//   <div>
+//     <Calendar onChange={onChange} value={value} />
+//   </div>
+// );
+
+// function TravelCalendar() {
+//   const [date, setDate] = useState(new Date());
+
+//   return (
+//     <div className="calendar">
+//       <h1 className="calendar-title">Travel Calendar</h1>
+//       <div className="calendar-container">
+//         <Calendar onChange={setDate} value={date} />
+//       </div>
+//       <p className="result-text">
+//         <span className="bold">Selected Date:</span> {date.toDateString()}
+//       </p>
+//     </div>
+//   );
+// }
+
+// Extended broken code
+// return (
+//   <div className="travel-calendar-section">
+//     <h1 className="text-center">React Calendar with Range</h1>
+//     <div className="calendar-container">
+//       <Calendar onChange={onChange} value={date} selectRange={true} />
+//     </div>
+//     {date.length > 0 ? (
+//       <p className="text-center">
+//         <span className="bold">Start:</span> {date[0].toDateString()}
+//         &nbsp;|&nbsp;
+//         <span className="bold">End:</span> {date[1].toDateString()}
+//       </p>
+//     ) : (
+//       <p className="text-center">
+//         <span className="bold">Default selected date:</span>
+//         {date.toDateString()}
+//       </p>
+//     )}
+//   </div>
+// );
+
+// import React, { useState } from 'react';
+// import Calendar from 'react-calendar';
+
+// const CalendarView = () => {
+//   const [date, setDate] = useState(new Date());
+//   const onChange = date => setDate(date)
+
+//   return (
+//   <>
+//   <div>
+//     <Calendar onChange={onChange} value={date} />
+//     <p>{date.toLocaleDateString()}</p> </div> </> );
+
+// };
+
+// export default CalendarView;
